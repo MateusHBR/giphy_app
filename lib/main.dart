aee3f6ca294223fe211c6131ae1624543c4558d6
MateusHBR <mateusmorais.r@gmail.com>
2020-04-17 17:27:52 -0300

    configurando  um tema de cores ao app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:giphy_app/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        hintColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(
+            color: Colors.white,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          labelStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
